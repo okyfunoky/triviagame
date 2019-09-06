8161a98cfddcce357b93f895958b4d5e110ef25a
Michael Stevens <okyfunoky@hotmail.com>
2019-09-05 20:12:46 -0700

    End to end logic working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,28 +2,36 @@ $(document).ready(function () {
     var timerId;
     var isRunning;
 
-    window.onload = function () {
-        $('#trueButton').on("click", checkAnswer);
-        $('#falseButton').on("click", checkAnswer);
-    };
-
-    var questionAndAnwer = {
-        question: "How much wood could a wood chuck chuck, if a wood chuck could chuck wood?",
-        answer: true,
-        number: 0,
-    }
+    $('#trueButton').on("click", checkAnswer);
+    $('#falseButton').on("click", checkAnswer);
 
     var game = {
-        questionAndAnwers: {
-            questionOne: {
+        questionAndAnwers: [
+            questionOne = {
                 question: "How much wood could a wood chuck chuck, if a wood chuck could chuck wood?",
                 answer: true,
                 number: 0
             },
-        },
-        questionsAsked: [],
+            questionTwo = {
+                question: "What is the average air speed of a swallow?",
+                answer: true,
+                number: 1
+            },
+            questionThree = {
+                question: "An african swallow or a european swallow?",
+                answer: true,
+                number: 2
+            },
+            questionFour = {
+                question: "I don't know. Wait, what, nooooo?",
+                answer: true,
+                number: 3
+            },
+        ],
+        questionsAsked: 0,
         score: 0,
-        currentQuestion: ""
+        currentQuestion: "",
+        countDownTotal: 30, //seconds
     }
 
     function startGame() {
@@ -32,19 +40,46 @@ $(document).ready(function () {
 
 
     function choseQuestion() {
-        game.currentQuestion = game.questionAndAnwers.questionOne;
+        if(game.currentQuestion === ""){
+            game.currentQuestion = game.questionAndAnwers[0]; 
+        }else if(game.questionsAsked === 4){
+            console.log("here!")
+            displayScore();
+            return;
+        }
+        else{
+            game.currentQuestion = game.questionAndAnwers[game.currentQuestion.number+1]
+        }
+
         $('#question').text(game.currentQuestion.question);
-        game.questionsAsked.push(game.currentQuestion.number);
-        console.log("current question number" + game.questionsAsked[0])
+        game.questionsAsked++;
+
         if (!isRunning) {
             isRunning = true;
-            console.log("got here");
-            timerId = setTimeout(displayFailure, 3000);
+            timerId = setInterval(countDown, 1000);
         }
     }
 
-    function resetGame() {
+    function countDown(){
+        game.countDownTotal--;
+        if(game.countDownTotal === 0){
+            clearInterval(timerId);
+            isRunning = false;
+            game.countDownTotal = 30;
+            displayFailure();
+        }
+        if(isRunning){
+            $('#timer').text(game.countDownTotal);
+        }
 
+    }
+
+    function resetGame() {
+        game.countDownTotal = 30;
+        game.currentQuestion = "";
+        game.questionsAsked = 0;
+        game.score = 0;
+        startGame();
     }
 
     function displayTimeRemaining() {
@@ -54,7 +89,7 @@ $(document).ready(function () {
     function checkAnswer() {
         if (String(game.currentQuestion.answer) == this.value) {
             console.log("correct");
-            score++;
+            game.score++;
             displaySuccess();
         } else {
             console.log("incorrect");
@@ -63,23 +98,35 @@ $(document).ready(function () {
     }
 
     function displaySuccess() {
+        if (isRunning) {
+            clearInterval(timerId);
+            isRunning = false;
+            console.log("Success timer");
+        }
         //actually display success
+        $('#question').text("Success");
         setTimeout(choseQuestion, 3000);
     }
 
     function displayFailure() {
         if (isRunning) {
-            clearTimeout(timerId);
+            clearInterval(timerId);
             isRunning = false;
-        }
-        $('#question').text("Failure");
+            console.log("Failure timer");
+        }   
         //actually display failure
+        $('#question').text("Failure");
         setTimeout(choseQuestion, 3000);
-
     }
 
     function displayScore() {
-
+        console.log("display score: isRunning: " + isRunning);
+        if(isRunning){
+            clearInterval(timerId);
+            isRunning = false;
+        }
+        setTimeout(resetGame, 10000);
+        $('#score').text(game.score);
     }
 
     startGame();
